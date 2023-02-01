a9d6d926c388719eb1e23e53bbd866991cbe679d
Kevin Tran <kevin.tran.kt@gmail.com>
2023-02-01 07:27:02 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,23 @@ import requests
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 
-
 app = Flask(__name__)
 CORS(app)
 
 
+def get_bearer_token():
+    with open("bearer_token.txt", "r") as file:
+        return file.read().strip()
+
+
 @app.route("/get_records/<int:record_id>", methods=["GET"])
 def get_records(record_id):
-    header = {"Authorization": "Bearer 6f8a775e-a7b3-42dc-8f4d-aeb9306050e0"}
+    header = {"Authorization": f"Bearer {get_bearer_token()}"}
     data = {
         "id": record_id
     }
-    payload = {}
-    response = requests.request(
-        "GET", "https://personalization.us-west-2.hightouch.com/v1/collections/mycustomers/records/id/"+str(record_id), headers=header, data=payload)
-
+    response = requests.post(
+        "https://personalization.us-west-2.hightouch.com/v1/collections/mycustomers/records", headers=header, json=data)
     return jsonify(response.json())
 
 
